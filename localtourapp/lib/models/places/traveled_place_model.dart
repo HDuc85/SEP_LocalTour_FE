24ef4c04fcf27512e41d3486992bd0d1a81ea92f
HDuc85 <hoangduc8501@gmail.com>
2024-12-12 12:22:03 +0700

    Update traveled_place_model.dart

--- localtourapp/lib/models/places/traveled_place_model.dart
+++ localtourapp/lib/models/places/traveled_place_model.dart
@@ -16,14 +16,14 @@ class TraveledPlaceModel {
   });
   factory TraveledPlaceModel.fromJson(Map<String, dynamic> json) {
     return TraveledPlaceModel(
-      placeName: json['placeName']??'Unknown place name',
-      placePhotoDisplay: json['placePhotoDisplay'],
-      wardName: json['wardName'] ?? 'Unknown Ward',
-      firstVisitDate: json['firstVisitDate'] != null
-          ? DateTime.parse(json['firstVisitDate']).toUtc()
+      placeName: json['place']['placeName']??'Unknown place name',
+      placePhotoDisplay: json['place']['placePhotoDisplay'] ?? '',
+      wardName: json['place']['wardName'] ?? 'Unknown Ward',
+      firstVisitDate: json['place']['firstVisitDate'] != null
+          ? DateTime.parse(json['place']['firstVisitDate']).toUtc()
           : DateTime.fromMillisecondsSinceEpoch(0).toUtc(), // Default to Unix epoch if null
-      lastVisitDate: json['lastVisitDate'] != null
-          ? DateTime.parse(json['lastVisitDate']).toUtc()
+      lastVisitDate: json['place']['lastVisitDate'] != null
+          ? DateTime.parse(json['place']['lastVisitDate']).toUtc()
           : DateTime.fromMillisecondsSinceEpoch(0).toUtc(),
       traveledTimes: json['traveledTimes'] ?? 0,
     );
